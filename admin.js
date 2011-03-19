3a357c113806f99a8b3ad372350b0788ea99c790
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-19 17:25:40 +0000

    tagging 1.9.2
    git-svn-id: https://plugins.svn.wordpress.org/marctv-xbox-360voice-blog/tags/1.9.2@362201 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.js
@@ -0,0 +1,6 @@
+(function ($) {
+    $(document).ready(function($) {
+        $('.settings_page_xbox360voice #message').hide();
+        $('.settings_page_xbox360voice #message').prependTo(".settings_page_xbox360voice .wrap").fadeIn('fast');
+    });
+})(jQuery);
